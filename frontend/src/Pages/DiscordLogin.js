de01a7318b88ed3a69a601a46ecaf9782fa7ec49
Maaz Khan <85396969+maazkhansw5145@users.noreply.github.com>
2022-12-04 06:37:54 +0500

    update

--- frontend/src/Pages/DiscordLogin.js
+++ frontend/src/Pages/DiscordLogin.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { createClient } from "@supabase/supabase-js";
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
-import "./login.css";
-import { useHistory } from "react-router-dom";
-
+import "./DiscordLogin.css";
 const supabase = createClient(
   "https://izscxrhuaeahiecswtad.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6c2N4cmh1YWVhaGllY3N3dGFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk4MjExMjUsImV4cCI6MTk4NTM5NzEyNX0.JvoWJKvE3pzHx5rU7IRhR4pTmKfSFkjDwZxaNfPxLZQ"
 );
 
 function DiscordLogin(props) {
-  const history = useHistory();
   supabase.auth.onAuthStateChange(async (event) => {
     console.log(event);
     if (event !== "SIGNED_OUT") {
@@ -23,16 +20,17 @@ function DiscordLogin(props) {
   return (
     <div>
       <div
+        className="title"
         style={{
           display: "flex",
           marginBottom: "45px",
           justifyContent: "center",
         }}
-        className="title"
       >
         <h3>Profit Outcome</h3>
       </div>
-      <div style={{ width: "50%", margin: "auto" }}>
+      <div style={{ width: "50%", margin: "auto",marginTop:150 }}>
+        <hr />
         <Auth
           supabaseClient={supabase}
           appearance={{ theme: ThemeSupa }}
